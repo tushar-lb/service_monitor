4f7083bd30f8fa2efe4feef76f5e21a47831ab22
tusharraut1994 <galaxy.tusharraut@gmail.com>
2020-07-28 09:27:13 +0530

    Fixed response time in ms query

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/prometheus/client_golang v1.7.1
 	github.com/sirupsen/logrus v1.6.0
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/tools v0.0.0-20200727154626-eaaaedc6afa7 // indirect
+	golang.org/x/tools v0.0.0-20200727233628-55644ead90ce // indirect
 )
--- main.go
+++ main.go
@@ -9,18 +9,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const (
-	request200 = "https://httpstat.us/200"
-	request503 = "https://httpstat.us/503"
-	invalidURL = "https://tushartest:8080"
-)
-
 var (
 	sampleExternalURLUp = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "sample_external_url_up",
 	}, []string{"url"},
 	)
-	sampleExternalURLResponseMs = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+	sampleExternalURLResponseMs = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "sample_external_url_response_ms",
 	}, []string{"url"},
 	)
@@ -38,24 +32,27 @@ func getURLDetails(url string) {
 	if err != nil {
 		logrus.Errorf("error while checking url [%s] status, %v", url, err)
 	}
-	responseTime := int64(time.Since(start).Milliseconds())
+	responseTime := float64(time.Since(start).Milliseconds())
 	statusCode := resp.StatusCode
+	logrus.Infof("URL: [%s], Response time:[%d], Status code:[%d]", url, responseTime, statusCode)
 	if statusCode == 503 {
 		sampleExternalURLUp.With(prometheus.Labels{"url": url})
 	} else {
 		sampleExternalURLUp.With(prometheus.Labels{"url": url}).Inc()
 	}
-	sampleExternalURLResponseMs.With(prometheus.Labels{"url": url}).Observe(float64(responseTime))
-	logrus.Infof("URL: [%s], Response time:[%d], Status code:[%d]", url, responseTime, statusCode)
+	sampleExternalURLResponseMs.With(prometheus.Labels{"url": url}).Add(responseTime)
 }
 
 func main() {
 	logrus.Infof("internal-service-monitor started..")
-	logrus.Infof("200 success request:")
-	getURLDetails(request200)
-	logrus.Infof("503 service unreachable request:")
-	getURLDetails(request503)
-
+	// Define list of url's to check
+	URLsList := []string{"https://httpstat.us/200", "https://httpstat.us/503"}
+	logrus.Infof("URLs list: %s", URLsList)
+	for _, url := range URLsList {
+		getURLDetails(url)
+	}
+	// register /metrics http handler with promhttp
 	http.Handle("/metrics", promhttp.Handler())
+	// start listening and serving requests on port 100001
 	http.ListenAndServe(":10001", nil)
 }
